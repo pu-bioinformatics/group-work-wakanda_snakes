bae519c79e1e2a9c9c08b5fe2e77c5aed80e23e5
marthaluka <mawia.martha@gmail.com>
2019-03-27 12:44:52 +0300

    Wakanda group has added their forever script

---
+++ scripts/wakandaScript.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+
+"""
+This script is used to generate random DNA sequences of length not more than 80 nucleotides. 
+The input arguments ;
+  noSeqs: The user inputs the number sequences to be generated
+  lenSeq: The user inputs the length of sequences to be generated
+  outfile:  The user inputs the name of the file with the extension .fasta
+"""
+
+import sys   #This module interacts with linux sys 
+from random import choice  # This module generates random elements from a given sequence
+
+
+
+def wakandaSnakes (noSeqs,lenSeq,outfile):
+    """
+    Generates random DNA sequences in fasta format 
+    """
+    if int(lenSeq) <=80: #To limit length of sequence
+        with open (outfile, "w") as myfile:
+            for i in range (1,noSeqs+1):
+                header=">"+"snake_sequence"+str(i)  #fasta format header
+                myfile.write("%s\n" %header) #write header to new file
+                DNA=""                       #empty DNA string
+                for c in range (lenSeq):
+                    DNA+=choice("ATGC")     #defines contents of sequence and generates random sequences using choice 
+                myfile.write("%s\n" %DNA)
+    else:
+        print ("Length of sequence is longer than 80 nucleotides")
+
+
+noSeqs=int(sys.argv[1])
+lenSeq=int(sys.argv[2])
+outfile=sys.argv[3]
+wakandaSnakes (noSeqs,lenSeq,outfile)
